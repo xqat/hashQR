0fbd886f09adf8bf9d4a64dca4a5ceb3af3586b4
xqat <github@qrc-designer.com>
2013-06-10 16:39:59 +0200

    Change license to LGPL

--- hashqr.py
+++ hashqr.py
@@ -8,20 +8,19 @@
     Contact: info aet hashQR doet c0m
 
     This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License version 3
+    it under the terms of the GNU Lesser General Public License version 3
     as published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    GNU Lesser General Public License for more details.
 
     See http://www.gnu.org/licenses/ for a copy of the license.
 
 
     todo:
         replace dependencies
-        put on github
         improve performance of qrlib
 
     
